9210d1451d02e8f1de3dbad246f8cfd576b6b766
Matt <matt.mmor@gmail.com>
2024-11-24 20:40:24 +0000

    chore: Setting up run-paper

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,3 +1,5 @@
+import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
+
 plugins {
     id("vesper.base")
 }
@@ -10,4 +12,16 @@ dependencies {
     implementation(libs.triumph.commands)
 
     implementation(libs.serialization.hocon)
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.21.1")
+    }
+
+    bukkitPluginYaml {
+        main.set("dev.triumphteam.vesper.Vesper")
+        load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+        apiVersion = "1.21"
+    }
 }
